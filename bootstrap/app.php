035b984613e2a16f7785013a414e7dcbe230a648
timoyxgit <mayonsirelletimothy@gmail.com>
2026-05-08 17:08:10 +0800

    Cleaned up bootstrap configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,9 +10,10 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+    ->withMiddleware(function (Middleware $middleware) {
+        // This ensures Railway's proxy is trusted for HTTPS
+        $middleware->trustProxies(at: '*');
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
